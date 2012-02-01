00f9b811dd524108a3db7779778d8dafe71bba14
Stephan Tentrup <s.tentrup@gmx.net>
2012-02-02 00:03:12 +0100

    Check if login data is set at app startup

--- Einsatzserver/src/net/tentrup/einsatzserver/HomeScreenActivity.java
+++ Einsatzserver/src/net/tentrup/einsatzserver/HomeScreenActivity.java
@@ -6,6 +6,10 @@ import greendroid.widget.ActionBar;
 import greendroid.widget.ActionBarItem;
 import greendroid.widget.NormalActionBarItem;
 import net.tentrup.einsatzserver.comm.Communicator;
+import net.tentrup.einsatzserver.config.PreferenceKeys;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.Editor;
@@ -23,6 +27,8 @@ import android.widget.ListView;
  *
  */
 public class HomeScreenActivity extends GDListActivity {
+
+	private static final int ALERT_DIALOG_CONFIGURATION_ERROR = 1;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -42,14 +48,44 @@ public class HomeScreenActivity extends GDListActivity {
                 .newActionBarItem(NormalActionBarItem.class)
                 .setDrawable(new ActionBarDrawable(this, R.drawable.ic_action_bar_info)), R.id.action_bar_view_info);
 		setTitle(R.string.app_name);
-		// show EULA
-		new Eula(this).show();
 		// check if app was started in test mode
 		boolean testmode = getIntent().getBooleanExtra(Communicator.PREF_TESTMODE, false);
 		SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
 		Editor editor = prefs.edit();
 		editor.putBoolean(Communicator.PREF_TESTMODE, testmode);
 		editor.commit();
+
+		checkLoginData();
+		new Eula(this).show();
+	}
+
+	/**
+	 * Checks if username and password are set. Shows message otherwise.
+	 */
+	private void checkLoginData() {
+		SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+		if (prefs.getString(PreferenceKeys.CONFIGURATION_USERNAME, "").trim().length() < 1 
+				|| prefs.getString(PreferenceKeys.CONFIGURATION_PASSWORD, "").trim().length() < 1) {
+			showDialog(ALERT_DIALOG_CONFIGURATION_ERROR);
+		}
+	}
+
+	@Override
+	protected Dialog onCreateDialog(int id) {
+		if (id == ALERT_DIALOG_CONFIGURATION_ERROR) {
+			AlertDialog.Builder builder = new AlertDialog.Builder(this);
+			builder.setMessage(R.string.alert_configuration_error)
+			       .setCancelable(false)
+			       .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+			           public void onClick(DialogInterface dialog, int id) {
+							Intent intent = new Intent(HomeScreenActivity.this, ConfigurationActivity.class);
+							startActivity(intent);
+			           }
+			       });
+			AlertDialog alert = builder.create();
+			return alert;
+		}
+		return super.onCreateDialog(id);
 	}
 
 	@Override
