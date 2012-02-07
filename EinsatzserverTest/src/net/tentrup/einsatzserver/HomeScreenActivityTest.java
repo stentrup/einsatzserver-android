2695978181b5d9083436b046a6c7a8ea3e62ab11
Stephan Tentrup <s.tentrup@gmx.net>
2012-02-08 00:10:34 +0100

    Add activity test

---
+++ EinsatzserverTest/src/net/tentrup/einsatzserver/HomeScreenActivityTest.java
@@ -0,0 +1,32 @@
+package net.tentrup.einsatzserver;
+
+import android.test.ActivityInstrumentationTestCase2;
+import android.widget.ListView;
+
+/**
+ * Activity test case for {@link HomeScreenActivity}.
+ *
+ * @author Tentrup
+ *
+ */
+public class HomeScreenActivityTest extends ActivityInstrumentationTestCase2<HomeScreenActivity> {
+
+	private HomeScreenActivity m_activity;
+	private ListView m_listView;
+
+	public HomeScreenActivityTest() {
+		super("net.tentrup.einsatzsever", HomeScreenActivity.class);
+	}
+
+	@Override
+	protected void setUp() throws Exception {
+		super.setUp();
+		setActivityInitialTouchMode(false);
+		m_activity = getActivity();
+		m_listView = (ListView) m_activity.findViewById(android.R.id.list);
+	}
+
+	public void testPreConditions() {
+		assertEquals(3, m_listView.getChildCount());
+	}
+}
